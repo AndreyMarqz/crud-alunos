7ab6ba637532d7846368f40738d06b8859a534c5
Andrey Marques <andreymmarques6@gmail.com>
2026-06-21 20:40:29 -0300

    feat: adiciona backend

---
+++ backend/src/main/java/io/github/AndreyMarqz/crudAlunos/CrudAlunosApplication.java
@@ -0,0 +1,13 @@
+package io.github.AndreyMarqz.crudAlunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAlunosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/AndreyMarqz/crudAlunos/CrudAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.AndreyMarqz.crudAlunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
